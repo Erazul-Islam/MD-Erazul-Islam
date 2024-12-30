1939d2f626da6f8648f13b64d1923c02dc2c7681
Erazul Islam <erazul89@gmail.com>
2024-12-30 21:57:04 +0600

    improve journey section

--- components/ui/about-ui/Journey.tsx
+++ components/ui/about-ui/Journey.tsx
@@ -14,6 +14,27 @@ const Journey = () => {
     });
   }, []);
 
+  const journeys = [
+    {
+      date: 'June 2023',
+      title: 'Started to learn HTML and CSS',
+      description:
+        'In my journey to becoming a developer, I began with HTML and CSS, the building blocks of web development. Learning how to structure web pages with HTML and style them using CSS was both exciting and foundational, setting the stage for creating visually appealing and user-friendly websites.',
+    },
+    {
+      date: 'July 2023',
+      title: 'Completed My First Frontend Project',
+      description:
+        'In July 2023, I completed my first frontend project. It was a challenging but rewarding experience that enhanced my skills in React, CSS, and JavaScript.',
+    },
+    {
+      date: 'August 2023',
+      title: 'Explored Backend Development',
+      description:
+        'By August 2023, I ventured into backend development, working with Node.js, Express, and MongoDB to build RESTful APIs and manage databases efficiently.',
+    },
+  ];
+
   return (
     <div className="container mx-auto p-8 md:p-16 ">
       <motion.h1
@@ -25,79 +46,46 @@ const Journey = () => {
         My Career Journey
       </motion.h1>
       <div className="space-y-20">
-        <motion.div
-          data-aos="fade-up"
-          data-aos-delay={`${200}`}
-          className=" flex-col md:flex-row items-center justify-center gap-10"
-        >
+        {journeys.map((journey, index) => (
           <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ delay: 0.5 }}
-            className="border border-pink-800 text-white p-6 rounded-md shadow-lg flex items-center justify-center w-20 h-20"
+            key={index}
+            data-aos={index % 2 === 0 ? 'fade-right' : 'fade-left'}
+            className={`flex flex-col ${
+              index % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse'
+            } items-center gap-10`}
           >
-            <span className="text-xl font-serif">June 2023</span>
-          </motion.div>
-
-          <div className="md:w-1/2 mt-4">
-            <motion.h3
-              initial={{ opacity: 0, x: -100 }}
-              animate={{ opacity: 1, x: 0 }}
+            <motion.div
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
               transition={{ delay: 0.5 }}
-              className="text-2xl font-serif text-pink-800"
+              className="border border-pink-800 text-white p-6 rounded-md shadow-lg flex items-center justify-center w-20 h-20"
             >
-              tarted to Create my first project
-            </motion.h3>
-            <motion.p
-              initial={{ opacity: 0, x: 100 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ delay: 0.7 }}
-              className="text-lg text-white mt-2"
-            >
-              I embarked on my journey to become a full-stack developer in June 2023. Motivated by a deep interest in technology and problem-solving, I started with web development and progressively built a strong foundation in front-end and back-end technologies
-            </motion.p>
-          </div>
-        </motion.div>
-        <motion.div
-          data-aos="fade-right"
-          data-aos-delay={`${200}`}
-          className=" flex-col md:flex-row items-right justify-right gap-10"
-        >
-          <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ delay: 0.5 }}
-            className="border border-pink-800 text-white p-6 rounded-md shadow-lg flex items-center justify-center w-20 h-20"
-          >
-            <span className="text-xl font-serif">June 2023</span>
+              <span className="text-xl font-serif">{journey.date}</span>
+            </motion.div>
+
+            <div className="md:w-1/2 mt-4">
+              <motion.h3
+                initial={{ opacity: 0, x: index % 2 === 0 ? -100 : 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ delay: 0.5 }}
+                className="text-2xl font-serif text-pink-800"
+              >
+                {journey.title}
+              </motion.h3>
+              <motion.p
+                initial={{ opacity: 0, x: index % 2 === 0 ? 100 : -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ delay: 0.7 }}
+                className="text-lg font-serif text-white mt-2"
+              >
+                {journey.description}
+              </motion.p>
+            </div>
           </motion.div>
-
-          <div className="md:w-1/2 mt-4">
-            <motion.h3
-              initial={{ opacity: 0, x: -100 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ delay: 0.5 }}
-              className="text-2xl font-serif text-pink-800"
-            >
-              tarted to Create my first project
-            </motion.h3>
-            <motion.p
-              initial={{ opacity: 0, x: 100 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ delay: 0.7 }}
-              className="text-lg text-white mt-2"
-            >
-              I embarked on my journey to become a full-stack developer in June 2023. Motivated by a deep interest in technology and problem-solving, I started with web development and progressively built a strong foundation in front-end and back-end technologies
-            </motion.p>
-          </div>
-        </motion.div>
-
+        ))}
       </div>
-
-
     </div>
   );
 };
 
 export default Journey;
-
