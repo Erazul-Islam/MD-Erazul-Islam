7356226cd3cc519da1493d0a362ac9b727c41dca
Erazul Islam <erazul89@gmail.com>
2024-12-31 23:38:53 +0600

    add daynamic page

---
+++ app/@blogs/[id]/page.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+const BlogDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BlogDetails;
